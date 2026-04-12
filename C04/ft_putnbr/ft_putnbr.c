f7bf9b14ffc65c29a06f8e21dcf1c824856d9743
Muaetih <mathieubenhaim@gmail.com>
2026-04-12 10:24:13 +0200

    exo 2

--- C04/ft_putnbr/ft_putnbr.c
+++ C04/ft_putnbr/ft_putnbr.c
@@ -30,14 +30,14 @@ void    ft_putnbr(int nb)
 int main(void)
 {
     ft_putnbr(0);
-    write(1, "\n", 1);
+    ft_putchar('\n');
     ft_putnbr(42);
-    write(1, "\n", 1);
+    ft_putchar('\n');
     ft_putnbr(-42);
-    write(1, "\n", 1);
+    ft_putchar('\n');
     ft_putnbr(2147483647);
-    write(1, "\n", 1);
+    ft_putchar('\n');
     ft_putnbr(-2147483648);
-    write(1, "\n", 1);
+    ft_putchar('\n');
     return (0);
 }
