fe010d1571fb4b39268661cbd12b53ef774b1dce
Muaetih <mathieubenhaim@gmail.com>
2026-04-13 09:45:51 +0200

    exo 3

---
+++ C05/ft_recursive_power/ft_recursive_power.c
@@ -0,0 +1,19 @@
+int ft_recurs(int nb, int power, int n)
+{
+
+    if (power >= 1)
+        return (ft_recurs(nb, power - 1, n * nb));
+    return nb;
+
+}
+
+int ft_recursive_power(int nb, int power)
+{
+    if (power < 0)
+        return 0;
+    if (power == 0)
+        return 1;
+    if (power == 1)
+        return nb;
+    return ft_recurs(nb, power, nb);
+}
