c7c4a1baa8bb2eaa5bc0f078ac7b233ea559029a
Muaetih <mathieubenhaim@gmail.com>
2026-04-13 10:09:58 +0200

    exo 4

---
+++ C05/ft_fibonacci/ft_fibonacci.c
@@ -0,0 +1,11 @@
+int ft_fibonnaci(int index)
+{
+    if ( index < 0)
+        return -1;
+    if (index == 0)
+        return 0;
+    if (index == 1)
+        return 1;
+    else
+        return ((ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+}
