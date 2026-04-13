92c550d63349f410a191d5ea172a53606e61c0fe
Muaetih <mathieubenhaim@gmail.com>
2026-04-13 09:16:50 +0200

    exo 0

---
+++ C05/ft_iterative_factorial/ft_iterative_factorial.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int ft_iterative_factorial(int nb)
+{
+    int res;
+    int i;
+
+    res = 1;
+    i = 1;
+    if (nb < 0)
+        return 0;
+    if (nb == 0)
+        return 1;
+    while (i <= nb)
+    {
+        res= res * i;
+        i++;
+    }
+    return res;
+}
+
+int main(void)
+{
+    printf("%d\n", ft_iterative_factorial(0));
+    printf("%d\n", ft_iterative_factorial(1));
+    printf("%d\n", ft_iterative_factorial(5));
+    printf("%d\n", ft_iterative_factorial(10));
+    printf("%d\n", ft_iterative_factorial(-1));
+    return (0);
+}
